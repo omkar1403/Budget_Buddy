91eae879746be3d9a40559907e44f3169413bc98
Omkar Deshmukh <deshmukhomkar144@gmail.com>
2024-06-25 19:21:34 +0530

    first commit

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className=" bg-white text-light p-5 sticky">
+      <h6 className="text-center">All rights reserved &copyright; Omkar</h6>
+    </div>
+  );
+};
+
+export default Footer;
